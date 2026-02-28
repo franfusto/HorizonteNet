420694d0d7f474232f0b384b63f12cb4cdb2be72
Franfusto <franfusto@gmail.com>
2026-02-28 19:28:54 +0100

    .save

--- Horizonte.WorkFlows/NodeModels/NodeOperationModel.cs
+++ Horizonte.WorkFlows/NodeModels/NodeOperationModel.cs
@@ -4,10 +4,21 @@ using Horizonte.Extension.WorkFlows;
 
 namespace Horizonte.WorkFlows.NodeModels;
 
+/// <summary>
+/// 
+/// </summary>
 public class NodeOperationModel : NodeModel
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    public WorkFlowNode Node { get; }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="node"></param>
     public NodeOperationModel(WorkFlowNode node) : base(node.Id, new Point(node.PosX, node.PosY))
     {
-        
+        Node = node;  
     }
 }
--- Horizonte.WorkFlows/NodeModels/NodeOutModel.cs
+++ Horizonte.WorkFlows/NodeModels/NodeOutModel.cs
@@ -3,10 +3,22 @@ using Blazor.Diagrams.Core.Models;
 using Horizonte.Extension.WorkFlows;
 namespace Horizonte.WorkFlows.NodeModels;
 
+/// <summary>
+/// 
+/// </summary>
 public class NodeOutModel : NodeModel
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    public WorkFlowNode Node { get; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="node"></param>
     public NodeOutModel(WorkFlowNode node) : base(node.Id, new Point(node.PosX, node.PosY))
     {
-        
+        Node = node;  
     }
 }
