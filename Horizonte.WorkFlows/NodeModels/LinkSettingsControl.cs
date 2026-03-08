ab01d205d6d128aba94e08f26bd368dea3678e30
Franfusto <franfusto@gmail.com>
2026-03-08 18:02:03 +0100

    .save

--- Horizonte.WorkFlows/NodeModels/LinkSettingsControl.cs
+++ Horizonte.WorkFlows/NodeModels/LinkSettingsControl.cs
@@ -15,10 +15,20 @@ namespace Horizonte.WorkFlows.NodeModels;
 
 
 
-public class LinkSettingsControl(WorkFlowLink LinkDef,WorkFlowDef workFlowDef) : ExecutableControl
+public class LinkSettingsControl : ExecutableControl
 {
 
+    public WorkFlowDef WorkFlowDef { get; private set; }
+    public WorkFlowLink WorkFlowLink { get; private set; }
     public Rectangle Bounds { get; private set; } = Rectangle.Zero;
+    public Diagram Diagram { get; private set; }
+
+    public LinkSettingsControl(WorkFlowLink LinkDef,WorkFlowDef workFlowDef, Diagram diagram)
+    {
+        WorkFlowDef = workFlowDef;
+        WorkFlowLink = LinkDef;
+        Diagram = diagram;
+    }
     
     public override Point? GetPosition(Model model)
     {
@@ -40,13 +50,13 @@ public class LinkSettingsControl(WorkFlowLink LinkDef,WorkFlowDef workFlowDef) :
         //throw new NotImplementedException();
     }
 
-    public void EditCondition()
+    public void EditCondition(WorkFlowLink updLink)
     {
-        
+        WorkFlowLink = updLink;
     }
 
     public void DeleteCondition()
     {
-        
+        WorkFlowLink.Condition = null;
     }
 }
